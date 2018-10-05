9b69a096899152dc98366451257e7bd38b5b1cd6
KurisuLim <kazuki1002003@yahoo.com>
2018-10-05 03:01:53 -0700

    first commit

---
+++ src/components/TodoInput.vue
@@ -0,0 +1,55 @@
+<template>
+    <div>
+        <input 
+        type="text"
+        class="input"
+        placeholder="New Todo"
+        v-model="inputEntry"
+        required
+        />
+        <button
+        class="btn"
+        @click="submitEvent(inputEntry)"
+        >
+        Submit
+        </button>
+        <p style="color: red; font-size: 13px;" v-if="error">
+            You must enter something first!
+        </p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'TodoInput',
+    data(){
+        return{
+            inputEntry: '',
+            error: false
+        }
+    },
+    methods: {
+        submitEvent (todoListDetail){
+            if(todoListDetail === '') return this.error = true;
+            console.log(this.inputEntry)
+        }
+    }
+}
+</script>
+//scoped means this style only exist in this component
+<style lang="scss" scoped>
+@import '../../public/variables.scss/variables.scss';
+
+.input{
+    width: 100%;
+    padding: 8px 10px;
+    border: 1px solid $vue-blue;
+}
+.btn{
+    margin: 10px auto;
+    padding: 5px 10%;
+    border: 1px solid $vue-blue;
+}
+
+</style>
+
---
+++ src/components/TodoList.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <TodoInput 
+    />
+  </div>
+  
+</template>
+
+<script>
+import TodoInput from './TodoInput.vue';
+
+export default {
+  name: 'TodoList',
+  components: {
+    TodoInput,
+  }
+  
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+
+</style>
